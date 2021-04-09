3f9d0c24953bf0f8b5142eed3d22edb486b5d7dc
Unknown <orelien.mary@gmail.com>
2021-04-09 17:08:04 +0200

    Pun

---
+++ Showdown/Assets/Scripts/PMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PMovement : MonoBehaviourPun
+{
+    [HideInInspector] public float horizontal;
+    [HideInInspector] public float vertical;
+
+    public float speed;
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
+        }
+
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontal, 0f, vertical).normalized * speed;
+    }
+}
